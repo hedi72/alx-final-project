adcfbe9ee76f78b5eaab3c2b597b6b77a3a085da
hedi72 <heditaieb72@gmail.com>
2024-08-21 00:22:36 +0100

    fixed context api

---
+++ src/context/Newcontext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+
+// Create the context
+const UserContext = createContext();
+
+// Create the context provider
+export const UserProvider = ({ children }) => {
+  const [user2, setUser2] = useState(null);
+
+  const update_serv = (username) => {
+    setUser2({ username });
+  };
+
+  return (
+    <UserContext.Provider value={{ user2, update_serv }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
---
+++ src/context/UserContext.js
@@ -0,0 +1,80 @@
+// UserContext.js
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+
+  const createUser = async (email, password, firstName, lastName) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      // Update user's display name (if name is provided)
+      if (firstName) {
+        await updateProfile(userCredential.user, {
+          displayName: firstName + " " + lastName,
+        });
+      }
+
+      return userCredential;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [auth]);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    logOut,
+    signInWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
